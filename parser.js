824610b9c66fd212759300bfffb8911b82582462
stanislave <staskachaev205@mail.ru>
2024-03-19 20:33:41 +0700

    чтоб не слетело

--- parser.js
+++ parser.js
@@ -1,8 +1,46 @@
+const formCreate = document.getElementById('formCreate');
+const nameForm = document.getElementById('nameForm');
+
 let fileURL;
+let data;
+
+// input.setAttribute('placeholder', if(!item.input.placeholder){ item.input.placeholder} else{ " "});
+let createFields = (data, form) =>{
+
+    data.fields.forEach((item) => {
+        let block = document.createElement('div');
+        block.setAttribute('style', 'display: flex; flex-direction: column');
+        let label = document.createElement('label');
+        label.innerHTML = item.label;
+        block.appendChild(label);
+        let input = document.createElement('input');
+        input.setAttribute('placeholder', (item.input.placeholder) ? item.input.placeholder : '');
+        input.setAttribute('type', item.input.type);
+        input.setAttribute('required', item.input.required);
+        input.setAttribute('mask', item.input.mask);
+        input.innerHTML = item.input;
+        block.appendChild(input);
+        form.appendChild(block);
+
+        // if (data.name === "interview"){
+        //     input.setAttribute('placeholder', '');
+        // }
+    })
+
+}
 async function showFile(input) {
     let file = input.files[0];
     fileURL = URL.createObjectURL(file);
     let response = await fetch(fileURL);
-    console.log(await response.json());
-};
+    data = await response.json();
+    console.log(data);
+    nameForm.innerHTML = data.name;
+    // title.setAttribute('style', 'color: red;')
+    // data.buttons.forEach((item) => {
+    //     let btn = document.createElement('button')
+    //     btn.innerHTML = item.text;
+    //     formCreate.appendChild(btn);
+    // })
+    createFields(data, formCreate);
 
+}
