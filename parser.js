f0a34106d3fccd4ab55cac2331bbdffc15772ae2
stanislave <staskachaev205@mail.ru>
2024-04-20 14:30:40 +0700

    color 80%

--- parser.js
+++ parser.js
@@ -31,22 +31,44 @@ let createFields = (data, form) =>{
             console.log(item);
         })) : '';
 
+        let colors = item.input.colors ?? null;
+        let blockcolor = document.createElement('form');
+        blockcolor.setAttribute('style', 'display: flex; flex-direction: column; height: fit-content; gap:15px;');
+        colors ? (colors.forEach((item) =>{
+            let colorblock = document.createElement('div');
+            colorblock.setAttribute('width','100%');
+            colorblock.setAttribute('style', `background-color: ${item}; height: 100px;`);
+            blockcolor.appendChild(colorblock);
+            let input = document.createElement('input');
+            input.setAttribute('type', 'radio');
+            input.setAttribute('name', 'color;');
+            blockcolor.appendChild(input);
+            console.log(item);
+        })) : '';
+
+
 
         for (const [attr, attrValue] of Object.entries(item.input)) {
             input.setAttribute(attr, attrValue);
             console.log(attr, attrValue, '\n\n');
 
-            let placeholder = item.input.placeholder ?? item.input.mask ?? null;
-            placeholder ? input.setAttribute('placeholder', placeholder) : null;
-
             let filetype = item.input.filetype ?? null;
             filetype ? input.setAttribute('accept', filetype.map(item => '.' + item).join(',')) : null;
 
-            let technology = item.input.type === "technology";
-            !technology ? input.setAttribute('type', item.input.type) : null;
+            let placeholder = item.input.placeholder ?? item.input.mask ?? null;
+            placeholder ? input.setAttribute('placeholder', placeholder) : null;
 
+            let technology = item.input.type === "technology";
+
+            !technology ? input.setAttribute('type', item.input.type) :  null;
             !technology ? block.appendChild(input) : block.appendChild(select);
+
+            let color = item.input.type === "color";
+            !color ? input.setAttribute('type', item.input.type) : null;
+            !color ? block.appendChild(input) : block.appendChild(blockcolor);
+
         }
+
 
         input.innerHTML = item.input;
         form.appendChild(block);
@@ -68,6 +90,4 @@ async function showFile(input) {
     // })
 
     createFields(data, formCreate);
-
-
 }
