59ec1759f616338663ff39d224a24e1bdd3cfba0
Ka4aev <staskachaev205@mail.ru>
2024-08-05 19:37:39 +0700

    daughter comments and comment-button

---
+++ sprint333/src/components/comment/CommentList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+import CommentItem from './CommentItem.vue'; // Import the item component
+
+defineProps({
+  comments: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div>
+    <CommentItem v-for="comment in comments" :key="comment.id" :commentId="comment" />
+  </div>
+</template>
--- sprint333/src/components/comment/PostComment.vue
+++ sprint333/src/components/comment/PostComment.vue
@@ -2,6 +2,7 @@
 import {newsTime} from "@/functions.js";
 import {onMounted, ref} from "vue";
 import {NewsServices} from "@/api/newsServices.js";
+import CommentList from "@/components/comment/CommentList.vue";
 
 const props = defineProps({
   commentId:{
@@ -11,16 +12,21 @@ const props = defineProps({
 console.log(props.commentId)
 
 const comment = ref()
+const showReplies = ref(false);
 
 const getComment = () => {
   NewsServices.getNews(props.commentId)
-      .then(data => {comment.value = data; console.log(data)})
+      .then(data => comment.value = data)
       .catch(error => console.log(error));
 }
 
 onMounted(() => {
   getComment();
 })
+
+const toggleReplies = () => {
+  showReplies.value = !showReplies.value; // Переключение видимости
+};
 </script>
 
 <template>
@@ -32,15 +38,31 @@ onMounted(() => {
     </div>
     <div class="comment-bottom" v-html="comment.text"></div>
 
+    <button @click="toggleReplies" class="reply-button">
+      {{ showReplies ? 'Hide comments' : 'Show comments' }}
+    </button>
+
+    <div v-if="showReplies">
+      <comment-list  :comment="comment" :comments="comment.kids"/>
+    </div>
+
   </article>
 </template>
 
 <style scoped lang="scss">
-a:hover{
+a,button:hover{
   text-decoration: underline;
 }
+.reply-button {
+  cursor: pointer;
+  color: gray;
+  font-family: $default-font;
+  background: none;
+  padding: 3px;
+}
+
 .comment{
-  padding: 10px 15px;
+  padding: 15px;
   width: fit-content;
 
   &-top{
